bf70f3d96cb6edc658091c18b60732c57d85ec55
Akilbek <akilbekurinbaev0616@gmail.com>
2024-07-11 22:17:54 +0500

    Finish

--- src/Components/VerificationForm/VerificationForm.jsx
+++ src/Components/VerificationForm/VerificationForm.jsx
@@ -4,6 +4,9 @@ import back from "../../assets/back.svg"
 export default function VerificationForm(){
 
     const navigate = useNavigate();
+    function handleClick(){
+        navigate("/");
+    }
     return(<>
         <button onClick={() => navigate(-1)} className="goback">
                 <img src={back} alt="Назад" style={{width:"28px"}}/>
@@ -12,6 +15,7 @@ export default function VerificationForm(){
             <div className="login">
                 <h2 className="go_reg">Выслали письмо со ссылкой для завершения регистрации на вашу почту.</h2>
                 <p style={{fontSize:"20px"}} className="go_reg">Если письмо не пришло, не спеши ждать совиную почту - лучше проверь ящик “Спам” <br></br>(´｡• ω •｡`)</p>
+                <button onClick={handleClick}>Авторизоватся</button>
             </div>
     </>)
 }
